2c39528a9995811666f23bbbbe371a9c8c55fc95
nitish <nitishxthakur@gmail.com>
2025-02-03 02:59:22 +0530

    update comment

--- dictionary.py
+++ dictionary.py
@@ -12,16 +12,20 @@ print(user['first_name'])
 positive_int={x:x**2 for x in range(10)}
 print(positive_int)
 
+#Accessing dictionary elements
 print(positive_int[5])
 print(positive_int.keys())
 
+#adding and deltion of items
 positive_int[11]=121   #add 11:121 key value pair
 del positive_int[1]    #delete 1 key value pair
 print(positive_int)
 
+#iteration over dictionary
 for y,z in positive_int.items():
     print(f'{y}:{z}')
 
+#dictionary comprihansion
 cube_val={x:x**3 for x in range(10)}
 print(cube_val)
 
